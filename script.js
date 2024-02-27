e3ebd453009d03332a3a02c5c1e3a8c8a7869de7
Vito <134098951+atomicgambit@users.noreply.github.com>
2024-02-27 07:16:46 +0100

    Adds index and js script

---
+++ script.js
@@ -0,0 +1,8 @@
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = "";
+
+//function operate
+//create a function that takes two numbers and an operator as argument
+//use a switch statement that evaluates operator and returns the calculation
+//make 4 case clauses, for add, subtract, multiply and divide.
