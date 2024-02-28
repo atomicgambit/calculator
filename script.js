069d98126117b4ffc6abd2ea0d538302a1bd15d6
Vito <134098951+atomicgambit@users.noreply.github.com>
2024-02-28 07:16:41 +0100

    Adds operate function to calculator

--- script.js
+++ script.js
@@ -2,7 +2,21 @@ let firstNumber = 0;
 let secondNumber = 0;
 let operator = "";
 
-//function operate
-//create a function that takes two numbers and an operator as argument
-//use a switch statement that evaluates operator and returns the calculation
-//make 4 case clauses, for add, subtract, multiply and divide.
+function operate(a, b, operator) {
+  let result = 0;
+  switch (operator) {
+    case "add":
+      result = a + b;
+      break;
+    case "subtract":
+      result = a - b;
+      break;
+    case "multiply":
+      result = a * b;
+      break;
+    case "divide":
+      result = a / b;
+      break;
+  }
+  return result;
+}
