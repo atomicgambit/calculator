86ddac8db6e5cb962cab2dcc2ae5ea735c7741a7
Vito <134098951+atomicgambit@users.noreply.github.com>
2024-03-01 09:41:47 +0100

    buttons show up on display when clicked

--- script.js
+++ script.js
@@ -20,3 +20,18 @@ function operate(a, b, operator) {
   }
   return result;
 }
+
+const displayedButtons = document.querySelectorAll(`button[type="button"]`);
+const display = document.querySelector("#display-text");
+displayedButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    display.textContent += button.value;
+  });
+});
+
+//Jezus, I guess it works now because I select the button itself instead of the class. For fucks sake.
+
+//Populating the display when you click the number buttons
+//Add a click event to every number button except the first row(that comes later)
+//When clicked, change the text content of text display, adding the new clicken content
+//Also when clicked, the variable should be stored
